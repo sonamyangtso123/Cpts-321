04958ed7594c97cb6c9aa0405e3afd41ce9d22c5
Sonam <sonam.yangtso@wsu.edu>
2020-09-16 16:01:32 -0700

    skeletal code and Unit test

---
+++ HW1/HW1/BSTNode.cs
@@ -0,0 +1,34 @@
+﻿namespace HW1
+{
+    public class BSTNode 
+    {
+        public int data; // private fields 
+        public BSTNode left;
+        public BSTNode right;
+
+        // constructor 
+        public BSTNode(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        // properties
+        //public int Data
+        //{
+        //    get {return this.data; }
+        //    set {this.data = value; }
+
+        //}
+        //public BSTNode Left
+        //{
+        //    get {return this.left; }
+        //    set {this.left = value; }
+        //}
+        //public BSTNode Right
+        //{
+        //    get { return this.right;}
+        //    set { this.right = value; }
+        //}
+    }
+}
---
+++ HW1/HW1/BSTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1
+{
+    public class BSTree
+    {
+        public BSTNode insert(BSTNode root, int number)
+        {
+            
+            return root;
+        }
+
+        public void inOrderTraverse( BSTNode root)
+        {
+            
+        }
+
+        public int countNumberOfNodes(BSTNode root)
+
+        {
+            return 0;
+        }
+
+        public int countNumberOflevels(BSTNode root)
+        {
+            return 0;
+        }
+
+
+       
+    }
+}
