41cb75fb3a0228bd3b158c8999ceb7d57d67f413
Sonam <sonam.yangtso@wsu.edu>
2020-12-10 00:50:22 -0800

    start working on homework-11 part1

---
+++ Homework_11/Homework_11/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Homework_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.urlDownloadButton = new System.Windows.Forms.Button();
+            this.resultDataTextBox = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(420, 34);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(317, 26);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // urlDownloadButton
+            // 
+            this.urlDownloadButton.Location = new System.Drawing.Point(428, 104);
+            this.urlDownloadButton.Name = "urlDownloadButton";
+            this.urlDownloadButton.Size = new System.Drawing.Size(309, 36);
+            this.urlDownloadButton.TabIndex = 1;
+            this.urlDownloadButton.Text = "\"Download string from URL\"";
+            this.urlDownloadButton.UseVisualStyleBackColor = true;
+            this.urlDownloadButton.Click += new System.EventHandler(this.URLDownLoadButton1_Click);
+            // 
+            // resultDataTextBox
+            // 
+            this.resultDataTextBox.Location = new System.Drawing.Point(434, 236);
+            this.resultDataTextBox.Multiline = true;
+            this.resultDataTextBox.Name = "resultDataTextBox";
+            this.resultDataTextBox.Size = new System.Drawing.Size(321, 250);
+            this.resultDataTextBox.TabIndex = 2;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(441, 20);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(42, 20);
+            this.Label1.TabIndex = 3;
+            this.Label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(445, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Download result (as string)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.resultDataTextBox);
+            this.Controls.Add(this.urlDownloadButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button urlDownloadButton;
+        private System.Windows.Forms.TextBox resultDataTextBox;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Homework_11/Homework_11/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void URLDownLoadButton1_Click(object sender, EventArgs e)
+        {
+            //disable UI components 
+            urlTextBox.Enabled = false;
+            resultDataTextBox.Enabled = false;
+            urlDownloadButton.Enabled = false;
+
+            // create an array of type byte to save the data from a resource
+            //byte[] data;
+            // instantaite an webClient class
+            WebClient cleint = new WebClient();
+            string resultData = string.Empty;
+
+            // create a new thread to download
+            Thread thread = new Thread(() =>
+                { 
+                    resultData = cleint.DownloadString(urlTextBox.Text);
+                    this.Invoke(new Action(() =>
+                    {
+                        resultDataTextBox.Text = resultData;
+                    }));
+                });
+            // start a thread
+            thread.Start();
+            // re-enable the UI compoments
+
+            urlTextBox.Enabled = true;
+            resultDataTextBox.Enabled = true;
+            urlDownloadButton.Enabled = true;
+
+        }
+
+
+    }
+}
